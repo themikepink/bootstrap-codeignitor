a7c8996b177dc3ee56f2aa292519db02aae5ad68
Mike Pink <mike.p@dnxmarketing.com>
2013-09-13 16:22:13 +0100

    removed welcome message and updates tabbing on views

--- application/views/welcome/index.php
+++ application/views/welcome/index.php
@@ -1,6 +1,6 @@
 <div class="container">
     <div class="row">
-        <div class="col-md-1"><span class="glyphicon glyphicon-search"></span></div>
+        <div class="col-md-1">.col-md-1</div>
         <div class="col-md-1">.col-md-1</div>
         <div class="col-md-1">.col-md-1</div>
         <div class="col-md-1">.col-md-1</div>
